2fcf17a24f56ba5052b428f19d6728258355672a
brunoderocco <brunoderocco@gmail.com>
2023-02-27 19:09:46 -0300

    Saving and showing transactions.

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -3,20 +3,29 @@
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
-import { createServer, Server } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import Modal from 'react-modal';
 import { useState } from 'react';
 import { NewTransactionModal } from './components/NewTransactionModal'
 
 createServer({
+    models: {
+        transaction: Model,
+    },
+
     routes(){
         this.namespace = "api";
+
         this.get("/transactions", () => {
-            return[
-                {id: 1, title: 'Transaction 1', amount : 400, type: 'deposit', category: 'food', CreatedAt: new Date()},
-                {id: 2, title: 'Transaction 2', amount : 800, type: 'debit', category: 'fun', CreatedAt: new Date()}
-            ]
+            return this.schema.all('transaction')
         })
+        
+        this.post('/transactions', (schema, request) =>{
+            const data = JSON.parse(request.requestBody)
+
+            return schema.create('transaction', data)
+        }
+        )
     },
 })
 
--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.png'
 import incomeImg from '../../assets/income.png'
 import outcomeImg from '../../assets/outcome.png'
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 //new test
 interface NewTransactionModalProps{
@@ -12,7 +13,23 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps) {
+    const[title, setTitle] = useState('');
+    const[value, setValue] = useState(0);
+    const[category, setCategory] = useState('');
+    
     const[type, setType] = useState ('none');
+
+    function handleCreateNewTransaction(event: FormEvent){
+        event.preventDefault();
+        const data = {
+            title,
+            value,
+            type,
+            category
+        };
+        
+        api.post('/transactions', data );
+    }
 
     return (
         <Modal isOpen={isOpen}
@@ -24,10 +41,18 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
                 <img src={closeImg} />
             </button>
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Register Transaction</h2>
-                <input placeholder='Title'/>
-                <input type="number" placeholder='Value'/>
+                
+                <input placeholder='Title'
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                />
+                
+                <input type="number" placeholder='Value'
+                    value={value}
+                    onChange={event=> setValue(Number(event.target.value))}
+                />
 
                 <TransactionTypeContainer>
                     <RadioBox type='button' 
@@ -48,7 +73,10 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
                     </RadioBox>
                 </TransactionTypeContainer>
 
-                <input placeholder='Category'/>
+                <input placeholder='Category'
+                    value={category}
+                    onChange={event=> setCategory(event.target.value)}
+                />
                 <button type='submit'>
                     Cadastrar
                 </button>
