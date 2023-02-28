fb5ee62abd3fbc6849c0d64b9f9490d2ff403037
brunoderocco <brunoderocco@gmail.com>
2023-02-28 17:08:33 -0300

    Modal data formating

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -3,31 +3,11 @@
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
-import { createServer, Model } from 'miragejs';
 import Modal from 'react-modal';
 import { useState } from 'react';
 import { NewTransactionModal } from './components/NewTransactionModal'
 
-createServer({
-    models: {
-        transaction: Model,
-    },
 
-    routes(){
-        this.namespace = "api";
-
-        this.get("/transactions", () => {
-            return this.schema.all('transaction')
-        })
-        
-        this.post('/transactions', (schema, request) =>{
-            const data = JSON.parse(request.requestBody)
-
-            return schema.create('transaction', data)
-        }
-        )
-    },
-})
 
 Modal.setAppElement('#root');
 
--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -2,6 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
+import { createServer, Model } from 'miragejs';
+
+createServer({
+    models: {
+        transaction: Model,
+    },
+
+    seeds(server){
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'FREELANCER',
+                    type: 'deposit',
+                    category: 'dev',
+                    amount: 6000,
+                    createdAt: new Date('2021-03-05 11:00:00')
+                },
+                {
+                    id: 2,
+                    title: 'RENT',
+                    type: 'withdraw',
+                    category: 'house',
+                    amount: 1000,
+                    createdAt: new Date('2023-03-10 11:00:00')
+                }
+            ]
+        })
+    },
+
+    routes(){
+        this.namespace = "api";
+
+        this.get("/transactions", () => {
+            return this.schema.all('transaction')
+        })
+        
+        this.post('/transactions', (schema, request) =>{
+            const data = JSON.parse(request.requestBody)
+
+            return schema.create('transaction', data)
+        }
+        )
+    },
+})
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
